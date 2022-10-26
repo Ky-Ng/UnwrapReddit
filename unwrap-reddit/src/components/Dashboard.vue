5f674a1c9f4dc39b830118c736cf87456d163308
kyleng <ng.kyle101@gmail.com>
2022-10-26 16:26:56 -0700

    Testing syntax of function passing from JS to Vue file

--- unwrap-reddit/src/components/Dashboard.vue
+++ unwrap-reddit/src/components/Dashboard.vue
@@ -1,58 +1,26 @@
 <template>
-  <v-card
-      class="mx-auto"
-      max-width="344"
-  >
-    <v-card-text>
-      <div>Word of the Day</div>
-      <p class="text-h4 text--primary">
-        el·ee·mos·y·nar·y
-      </p>
-      <p>adjective</p>
-      <div class="text--primary">
-        relating to or dependent on charity; charitable.<br>
-        "an eleemosynary educational institution."
-      </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn
-          text
-          color="teal accent-4"
-          @click="reveal = true"
-      >
-        Learn More
-      </v-btn>
-    </v-card-actions>
-
-    <v-expand-transition>
-      <v-card
-          v-if="reveal"
-          class="transition-fast-in-fast-out v-card--reveal"
-          style="height: 100%;"
-      >
-        <v-card-text class="pb-0">
-          <p class="text-h4 text--primary">
-            Origin
-          </p>
-          <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
-        </v-card-text>
-        <v-card-actions class="pt-0">
-          <v-btn
-              text
-              color="teal accent-4"
-              @click="reveal = false"
-          >
-            Close
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-expand-transition>
-  </v-card>
+  <v-main>
+    <v-btn @click="test">
+      "click this button"
+    </v-btn>
+  </v-main>
 </template>
 
 <script>
+import {printFromParser} from "@/parsers/parser";
+
 export default {
-  name: "Dashboard"
+  name: "Dashboard",
+  data: () => ({
+
+  }),
+  methods: {
+    test(s) {
+      console.log("saying hi from dashboard" + s)
+      printFromParser();
+    },
+
+  }
 }
 </script>
 
---
+++ unwrap-reddit/src/parsers/parser.js
@@ -0,0 +1,22 @@
+// const snoowrap = require('snoowrap');
+
+// const apiParser = new snoowrap({
+//     // userAgent: 'put your user-agent string here',
+//     userAgent: 'this is a description of my apiParser- Kyle Ng',
+//     clientId: 'qWhfH3k31_jr6awhEb3TSQ',
+//     clientSecret: 'aCsq5mE-nmuorGRyBGWYwAe9yLrpUw',
+//     // refreshToken: 'put your refresh token here'
+// });
+
+function printFromParser(){
+    console.log("hello from parser");
+}
+
+// function test() {
+//     console.log("hello world")
+//     apiParser.getSubreddit('AskReddit');
+//     console.log(apiParser.getSubreddit('AskReddit'));
+// // const rawRed}dit =
+// }
+
+export{printFromParser}
