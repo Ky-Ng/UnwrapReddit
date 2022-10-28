93f1e34f597a882451f6c9917bda6d599d2a2037
kyleng <ng.kyle101@gmail.com>
2022-10-27 18:36:45 -0700

    Added a graph

--- unwrap-reddit/src/views/Dashboard.vue
+++ unwrap-reddit/src/views/Dashboard.vue
@@ -1,31 +1,39 @@
 <template>
-  <v-main>
-    <v-row>
-      <v-col>
-        <redditSearch></redditSearch>
+  <div>
+    <redditSearch></redditSearch>
+    <v-row v-for="n in 3" :key="n">
+      <v-col cols="6">
+        <DataGraph></DataGraph>
       </v-col>
-      <v-btn @click="fetchPosts">
-        Fetch Reddit Posts
-      </v-btn>
-      <v-btn @click="printPostsToConsole">
-        Print Posts To Console
-      </v-btn>
-      <v-btn @click="printSubReddit('dogs')">
-        log whole subreddit
-      </v-btn>
     </v-row>
 
-  </v-main>
+
+  </div>
+<!--    <v-row>-->
+
+<!--      <v-btn @click="fetchPosts">-->
+<!--        Fetch Reddit Posts-->
+<!--      </v-btn>-->
+<!--      <v-btn @click="printPostsToConsole">-->
+<!--        Print Posts To Console-->
+<!--      </v-btn>-->
+<!--      <v-btn @click="printSubReddit('dogs')">-->
+<!--        log whole subreddit-->
+<!--      </v-btn>-->
+<!--    </v-row>-->
+
 </template>
 
 <script>
-import {fetchSubRedditPosts, redditPosts,getSubReddit} from "@/parsers/parser";
+import {fetchSubRedditPosts, redditPosts, getSubReddit} from "@/parsers/parser";
 import Search from "@/components/Search";
+import Graph from "@/components/Graph";
 
 export default {
   name: "Dashboard",
   components: {
     redditSearch: Search,
+    DataGraph: Graph,
   },
   data: () => ({}),
   methods: {
@@ -38,7 +46,7 @@ export default {
           redditpost => console.log(redditpost)
       );
     },
-    printSubReddit(name){
+    printSubReddit(name) {
       getSubReddit(name)
     },
   }
