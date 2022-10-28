7b243b20e95ff4ba163feabdb94ec898627e48f8
kyleng <ng.kyle101@gmail.com>
2022-10-27 19:31:46 -0700

    Updated the r/ formatting method

--- unwrap-reddit/src/components/Search.vue
+++ unwrap-reddit/src/components/Search.vue
@@ -19,7 +19,7 @@
     ></v-autocomplete>
     {{formatSearch}}
     <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
+      <v-icon @click="formatSearch">mdi-magnify</v-icon>
     </v-btn>
   </v-toolbar>
 </template>
@@ -34,73 +34,17 @@ export default {
       search: null,
       formatSearch: null,
       select: null,
-      states: [
-        'Alabama',
-        'Alaska',
-        'American Samoa',
-        'Arizona',
-        'Arkansas',
-        'California',
-        'Colorado',
-        'Connecticut',
-        'Delaware',
-        'District of Columbia',
-        'Federated States of Micronesia',
-        'Florida',
-        'Georgia',
-        'Guam',
-        'Hawaii',
-        'Idaho',
-        'Illinois',
-        'Indiana',
-        'Iowa',
-        'Kansas',
-        'Kentucky',
-        'Louisiana',
-        'Maine',
-        'Marshall Islands',
-        'Maryland',
-        'Massachusetts',
-        'Michigan',
-        'Minnesota',
-        'Mississippi',
-        'Missouri',
-        'Montana',
-        'Nebraska',
-        'Nevada',
-        'New Hampshire',
-        'New Jersey',
-        'New Mexico',
-        'New York',
-        'North Carolina',
-        'North Dakota',
-        'Northern Mariana Islands',
-        'Ohio',
-        'Oklahoma',
-        'Oregon',
-        'Palau',
-        'Pennsylvania',
-        'Puerto Rico',
-        'Rhode Island',
-        'South Carolina',
-        'South Dakota',
-        'Tennessee',
-        'Texas',
-        'Utah',
-        'Vermont',
-        'Virgin Island',
-        'Virginia',
-        'Washington',
-        'West Virginia',
-        'Wisconsin',
-        'Wyoming',
+      subredditTitles: [
+        'dogs',
+          'cats',
+          'games'
       ],
     }
   },
   watch: {
     search(val) {
       val && val !== this.select && this.querySelections(val)
-      this.formatSearch = "r/" + this.search;
+      this.formatSearch = this.search == null ? '' : 'r/' + this.search;
     },
   },
   methods: {
@@ -108,7 +52,7 @@ export default {
       this.loading = true
       // Simulated ajax query
       setTimeout(() => {
-        this.items = this.states.filter(e => {
+        this.items = this.subredditTitles.filter(e => {
           return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
         })
         this.loading = false
