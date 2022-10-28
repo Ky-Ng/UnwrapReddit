7df12819c94b4e090ce4909b78d9ddf8958fef48
kyleng <ng.kyle101@gmail.com>
2022-10-27 19:05:33 -0700

    Formatting the Grid

--- unwrap-reddit/src/views/Dashboard.vue
+++ unwrap-reddit/src/views/Dashboard.vue
@@ -1,29 +1,37 @@
 <template>
-  <div>
-    <redditSearch></redditSearch>
-    <v-row v-for="n in 3" :key="n">
-      <v-col cols="6">
+  <v-main
+  class="ps-1 pb-2">
+    <v-row class="pt-3">
+      <redditSearch></redditSearch>
+    </v-row>
+    <v-row v-for="n in 2" :key="n" class="mx-0">
+
+        <v-col v-for="n in 2" :key="n" cols="6">
+          <BestTime></BestTime>
+        </v-col>
+    </v-row>
+
+    <v-row v-for="n in 2" :key="n" class="mx-0">
+      <v-col v-for="n in 2" :key="n" cols="6">
         <DataGraph></DataGraph>
-      </v-col>
-      <v-col cols="3">
-        <BestTime></BestTime>
       </v-col>
     </v-row>
 
 
-  </div>
-<!--    <v-row>-->
 
-<!--      <v-btn @click="fetchPosts">-->
-<!--        Fetch Reddit Posts-->
-<!--      </v-btn>-->
-<!--      <v-btn @click="printPostsToConsole">-->
-<!--        Print Posts To Console-->
-<!--      </v-btn>-->
-<!--      <v-btn @click="printSubReddit('dogs')">-->
-<!--        log whole subreddit-->
-<!--      </v-btn>-->
-<!--    </v-row>-->
+  </v-main>
+  <!--    <v-row>-->
+
+  <!--      <v-btn @click="fetchPosts">-->
+  <!--        Fetch Reddit Posts-->
+  <!--      </v-btn>-->
+  <!--      <v-btn @click="printPostsToConsole">-->
+  <!--        Print Posts To Console-->
+  <!--      </v-btn>-->
+  <!--      <v-btn @click="printSubReddit('dogs')">-->
+  <!--        log whole subreddit-->
+  <!--      </v-btn>-->
+  <!--    </v-row>-->
 
 </template>
 
