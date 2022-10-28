ccbc3f5540933411cf967c9b7f95876155ba764a
kyleng <ng.kyle101@gmail.com>
2022-10-27 22:50:05 -0700

    Getting unused variable error

--- unwrap-reddit/src/parsers/parser.js
+++ unwrap-reddit/src/parsers/parser.js
@@ -2,7 +2,7 @@ const snoowrap = require('snoowrap');
 
 let redditPosts = [];
 let isValidReq = null;
-let isBusy = false;
+let isBusy = null;
 
 const apiParser = new snoowrap({
     userAgent: 'Unwrap Reddit API Parser- Kyle Ng',
@@ -16,55 +16,34 @@ const apiParser = new snoowrap({
 async function fetchSubRedditPosts(subRedditName) {
     // todo increase the number of posts to 500
     redditPosts = await apiParser.getTop(subRedditName, {time: "year", limit: 100});
-    console.log("done");
 }
 
-async function getSubReddit(subRedditName){
-    let subreddit = await apiParser.getSubreddit(subRedditName)
-    console.log(subreddit);
-}
-
-async function safeFetchSubRedditPosts(subRedditName){
-    await isValidSubreddit(subRedditName)
-    // console.log("isValidReq " + isValidReq); // todo remove
-    if (isValidReq){
-        console.log("just updated that isBusy is true");
+async function safeFetchSubRedditPosts(subRedditName) {
+    await isValidSubreddit(subRedditName);
+    if (isValidReq) {
         await fetchSubRedditPosts(subRedditName);
-        console.log("finsihed fetching 500 posts" + "is busy before update" + isBusy )
-        isBusy = false;
-        console.log("is busy after update " + isBusy);
-    } else {
-        isBusy = false;
     }
+    isBusy = false;
 }
 
 async function isValidSubreddit(subRedditName) {
     isBusy = true;
     await apiParser.getTop(subRedditName, {time: "year", limit: 1})
-        .then( (validPost) => {
-                console.log("Valid Post " + validPost); //todo remove
-                isValidReq = true;
+        .then((validPost) => {
+            console.log("Valid Post " + validPost);
+            isValidReq = true;
         })
-        .catch( (invalidPostError) => {
+        .catch((invalidPostError) => {
             console.log("Invalid Post " + invalidPostError); //todo remove
             isValidReq = false;
         });
 }
 
-// blocks the thread until isBusy becomes false
-function waitWhileParserBusy(){
-    console.log("before wait block waitWhileParserBusy " + isBusy)
-    while(isBusy){
-        console.log("in waitWhileParserBusy " + isBusy)
-        // block the thread
-    }
-}
-
-function printPostTitle(){
+function printPostTitle() {
     redditPosts.forEach(post => {
         console.log(post.title)
     });
 }
 
 
-export{printPostTitle, fetchSubRedditPosts, redditPosts, getSubReddit, isValidSubreddit, safeFetchSubRedditPosts, isBusy, waitWhileParserBusy}
+export {printPostTitle, safeFetchSubRedditPosts}
