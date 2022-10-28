463a2f5c6f6fff1cddc479d2a4f2ef7e12bcb671
kyleng <ng.kyle101@gmail.com>
2022-10-27 21:01:50 -0700

    isBusy boolean added

--- unwrap-reddit/src/parsers/parser.js
+++ unwrap-reddit/src/parsers/parser.js
@@ -2,6 +2,7 @@ const snoowrap = require('snoowrap');
 
 let redditPosts = [];
 let isValidReq = null;
+let isBusy = false;
 
 const apiParser = new snoowrap({
     userAgent: 'Unwrap Reddit API Parser- Kyle Ng',
@@ -12,8 +13,8 @@ const apiParser = new snoowrap({
     // refreshToken: 'put your refresh token here'
 });
 
-async function fetchSubRedditPosts() {
-    redditPosts = await apiParser.getTop('dogs', {time: "year", limit: 500});
+async function fetchSubRedditPosts(subRedditName) {
+    redditPosts = await apiParser.getTop(subRedditName, {time: "year", limit: 500});
     console.log("done");
 }
 
@@ -24,20 +25,24 @@ async function getSubReddit(subRedditName){
 
 async function safeFetchSubRedditPosts(subRedditName){
     await isValidSubreddit(subRedditName)
-    console.log("isValidReq " + isValidReq);
+    // console.log("isValidReq " + isValidReq); // todo remove
     if (isValidReq){
-        console.log("From safe fetch, this was a valid post");
+        isBusy = true;
+        await fetchSubRedditPosts(subRedditName);
+        console.log("finsihed fetching 500 posts" + "is busy before update" + isBusy )
+        isBusy = false;
+        console.log("is busy after update " + isBusy);
     }
 }
 
 async function isValidSubreddit(subRedditName) {
     await apiParser.getTop(subRedditName, {time: "year", limit: 1})
         .then( (validPost) => {
-                console.log("Valid Post " + validPost);
+                console.log("Valid Post " + validPost); //todo remove
                 isValidReq = true;
         })
         .catch( (invalidPostError) => {
-            console.log("Invalid Post " + invalidPostError);
+            console.log("Invalid Post " + invalidPostError); //todo remove
             isValidReq = false;
         });
 }
