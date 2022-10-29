d8d8518ee3035ac922cf1eaef35c343906b940ce
kyleng <ng.kyle101@gmail.com>
2022-10-29 16:51:36 -0700

    Modularize Components

---
+++ unwrap-reddit/src/components/user_input/SubRedditTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row class="mx-0">
+    <v-col>
+      <div class="indigo darken-1 text-center rounded">
+        <v-card-title class="white--text ">Analytics from subreddit:_<strong>r/{{ subRedditName }}</strong></v-card-title>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "SubRedditTitle",
+  props: {
+    subRedditName: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- unwrap-reddit/src/views/Dashboard.vue
+++ unwrap-reddit/src/views/Dashboard.vue
@@ -4,16 +4,10 @@
     <div v-if="renderedIn">
 
       <v-row class="mx-1">
-        <redditSearch @update-subreddit="onRedditUpdate($event)"></redditSearch>
+        <Search @update-subreddit="onRedditUpdate($event)"></Search>
       </v-row>
 
-      <v-row class="mx-0">
-        <v-col>
-          <div class="indigo darken-1 text-center rounded">
-            <v-card-title class="white--text ">Analytics from subreddit:_<strong>r/{{ subRedName }}</strong></v-card-title>
-          </div>
-        </v-col>
-      </v-row>
+      <SubRedditTitle :sub-reddit-name="subRedName"></SubRedditTitle>
 
       <v-row class="mx-0">
         <v-col cols="6">
@@ -52,14 +46,16 @@ import {safeFetchSubRedditPosts} from "@/parsers/parser";
 import Loader from "@/components/user_input/Loader";
 import {Analytics} from "@/parsers/analytics";
 import AnalyticsGraph from "@/components/graphs/AnalyticsGraph";
+import SubRedditTitle from "@/components/user_input/SubRedditTitle";
 
 export default {
   name: "Dashboard",
   components: {
-    redditSearch: Search,
+    Search,
     AnalyticsCard,
     AnalyticsGraph,
     Loader,
+    SubRedditTitle,
   },
   data: () => ({
     renderedIn: false,
