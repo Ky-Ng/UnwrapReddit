a47354a81704749b0eb088812a65cede6c8883b6
kyleng <ng.kyle101@gmail.com>
2022-10-29 16:54:40 -0700

    Aesthetics

--- unwrap-reddit/src/components/user_input/SubRedditTitle.vue
+++ unwrap-reddit/src/components/user_input/SubRedditTitle.vue
@@ -1,8 +1,14 @@
 <template>
-  <v-row class="mx-0">
+  <v-row class="ml-0 mr-2 mt-2">
     <v-col>
       <div class="indigo darken-1 text-center rounded">
-        <v-card-title class="white--text ">Analytics from subreddit:_<strong>r/{{ subRedditName }}</strong></v-card-title>
+        <v-row>
+          <v-col cols=""></v-col>
+          <v-col>
+            <v-card-title class="white--text ">Analytics from subreddit:_<strong>r/{{ subRedditName }}</strong></v-card-title>
+          </v-col>
+          <v-col cols=""></v-col>
+        </v-row>
       </div>
     </v-col>
   </v-row>
