ba44280829c38bcc43f8b6041bae914a21fce51c
kyleng <ng.kyle101@gmail.com>
2022-10-29 17:11:11 -0700

    Modularize DataDisplay Component

--- unwrap-reddit/src/components/DataDisplay/DataDisplay.vue
+++ unwrap-reddit/src/components/DataDisplay/DataDisplay.vue
@@ -1,10 +1,44 @@
 <template>
-  
+  <div>
+    <v-row class="mx-0">
+      <v-col cols="6">
+        <v-card color="indigo lighten-2" class="pa-1">
+          <DataCard :subRedditAnalyticsCategory="subRedditAnalytics.weekday"></DataCard>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card color="indigo lighten-2" class="pa-1">
+          <DataCard :subRedditAnalyticsCategory="subRedditAnalytics.hours"></DataCard>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row class="mx-0">
+      <v-col cols="6">
+        <v-card color="indigo lighten-2" class="pa-1">
+          <DataCard :subRedditAnalyticsCategory="subRedditAnalytics.weekday"></DataCard>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card color="indigo lighten-2" class="pa-1">
+          <DataCard :subRedditAnalyticsCategory="subRedditAnalytics.hours"></DataCard>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
+import DataCard from "@/components/DataDisplay/DataCard";
+
 export default {
-  name: "DataDisplay"
+  name: "DataDisplay",
+  components: {
+    DataCard,
+  },
+  props:{
+    subRedditAnalytics: Object,
+  }
 }
 </script>
 
--- unwrap-reddit/src/views/Dashboard.vue
+++ unwrap-reddit/src/views/Dashboard.vue
@@ -1,34 +1,12 @@
 <template>
-  <v-main
-      class="ps-1 pb-2">
-    <div v-if="renderedIn">
+  <v-main class="ps-1 pb-2">
 
-      <v-row class="mx-1">
-        <Search @update-subreddit="onRedditUpdate($event)"></Search>
-      </v-row>
+    <div v-if="renderedIn">
+      <v-row class="mx-1"><Search @update-subreddit="onRedditUpdate($event)"></Search></v-row>
 
       <SubRedditTitle :sub-reddit-name="subRedName"></SubRedditTitle>
 
-      <v-row class="mx-0">
-        <v-col cols="6">
-          <v-card color="indigo lighten-2" class="pa-1">
-            <DataCard :subRedditAnalyticsCategory="subRedditAnalytics.weekday"></DataCard>
-          </v-card>
-        </v-col>
-        <v-col cols="6">
-          <v-card color="indigo lighten-2" class="pa-1">
-            <DataCard :subRedditAnalyticsCategory="subRedditAnalytics.hours"></DataCard>
-          </v-card>
-        </v-col>
-      </v-row>
-
-      <v-row class="mx-0">
-        <v-col cols="6">
-
-        </v-col>
-        <v-col cols="6">
-        </v-col>
-      </v-row>
+      <DataDisplay :subRedditAnalytics="subRedditAnalytics"></DataDisplay>
     </div>
 
     <div v-else>
@@ -43,7 +21,7 @@ import {safeFetchSubRedditPosts} from "@/parsers/parser";
 import Loader from "@/components/user_input/Loader";
 import {Analytics} from "@/parsers/analytics";
 import SubRedditTitle from "@/components/user_input/SubRedditTitle";
-import DataCard from "@/components/DataDisplay/DataCard";
+import DataDisplay from "@/components/DataDisplay/DataDisplay";
 
 export default {
   name: "Dashboard",
@@ -51,7 +29,7 @@ export default {
     Search,
     Loader,
     SubRedditTitle,
-    DataCard,
+    DataDisplay,
   },
   data: () => ({
     renderedIn: false,
