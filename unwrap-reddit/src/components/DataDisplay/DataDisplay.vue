a6e814ffa49d80f4ecd11c3a939a7e824a4a1f06
kyleng <ng.kyle101@gmail.com>
2022-10-29 17:23:00 -0700

    Dynamic Rendering

--- unwrap-reddit/src/components/DataDisplay/DataDisplay.vue
+++ unwrap-reddit/src/components/DataDisplay/DataDisplay.vue
@@ -36,12 +36,64 @@ export default {
   components: {
     DataCard,
   },
-  props:{
+  props: {
     subRedditAnalytics: Object,
-  }
+  },
+  data: () => ({
+    analyticProperty: [
+      this.subRedditAnalytics.weekday,
+      this.subRedditAnalytics.hours,
+      this.subRedditAnalytics.weekday,
+      this.subRedditAnalytics.hours,
+    ],
+  }),
 }
 </script>
 
 <style scoped>
 
 </style>
+<!--Dynamic Rendering version-->
+<!--
+<template>
+  <div>
+    <v-row class="mx-0" v-for="property in subRedditAnalytics" :key="property">
+      <v-col cols="6">
+        <v-card color="indigo lighten-2" class="pa-1">
+          <DataCard :subRedditAnalyticsCategory="property"></DataCard>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card color="indigo lighten-2" class="pa-1">
+          <DataCard :subRedditAnalyticsCategory="property"></DataCard>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import DataCard from "@/components/DataDisplay/DataCard";
+
+export default {
+  name: "DataDisplay",
+  components: {
+    DataCard,
+  },
+  props: {
+    subRedditAnalytics: Object,
+  },
+  data: () => ({
+    analyticProperty: [
+      this.subRedditAnalytics.weekday,
+      this.subRedditAnalytics.hours,
+      this.subRedditAnalytics.weekday,
+      this.subRedditAnalytics.hours,
+    ],
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>-->
